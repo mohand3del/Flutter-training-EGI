ce9470c0591dd304158f0bb94bc235e8d31ced79
mohand3del <101574221+mohand3del@users.noreply.github.com>
2023-02-13 23:40:50 +0200

    Task-4

--- Task-4/Task4.dart
+++ Task-4/Task4.dart
@@ -1,127 +1,98 @@
 import 'dart:io';
 
-import 'package:task4/task4.dart' as task4;
+
+class Seat {
+  int? seatNumber;
+  SeatType? type;
+  bool? isAvailable;
+  
+  Seat(this.seatNumber, this.type) {
+    isAvailable = true;
+  }
+}
+
+enum SeatType {
+  FIRST_CLASS,
+  BUSINESS_CLASS,
+  ECONOMY_CLASS,
+}
+
+class Flight {
+  List<Seat>? seats;
+  
+  Flight() {
+    seats = [];
+    for (int i = 0; i < 50; i++) {
+      seats!.add(Seat(i + 1, SeatType.FIRST_CLASS));
+    }
+    for (int i = 50; i < 100; i++) {
+      seats!.add(Seat(i + 1, SeatType.BUSINESS_CLASS));
+    }
+    for (int i = 100; i < 150; i++) {
+      seats!.add(Seat(i + 1, SeatType.ECONOMY_CLASS));
+    }
+  }
+  
+  int reserveSeat(SeatType type) {
+    for (Seat seat in seats!) {
+      if (seat.type == type && seat.isAvailable!) {
+        seat.isAvailable = false;
+        return seat.seatNumber!;
+      }
+    }
+    return -1;
+  }
+  
+  void displaySeats() {
+    print("Seat Number\tType\tAvailability");
+    for (Seat seat in seats!) {
+     // print("$(seat.seatNumber)\t\t$(seat.type)\t\t$(seat.isAvailable ? 'Available' : 'Not Available')");
+    }
+  }
+}
 
 void main() {
-  //Hotel hotel = Hotel();
-
-  // hotel.makeReservation(DateTime.now(), DateTime.now().add(Duration(days: 3)),
-  //     TypeMyRoom.singlePerson);
-  // hotel.makeReservation(DateTime.now().add(Duration(days: 4)),
-  //     DateTime.now().add(Duration(days: 7)), TypeMyRoom.doublePerson);
-  Hotel hotel = Hotel();
+  Flight flight = Flight();
   
   while (true) {
-    print("\nHotel Reservation System\n");
-    print("1. Make a Reservation");
-    print("2. View Reservations"); //creat future
+    print("\nAirline Reservation System\n");
+    print("1. Reserve Seat");
+    print("2. View Seats");
     print("3. Exit");
     print("Enter your choice: ");
     
     int choice = int.parse(stdin.readLineSync()!);
     
     if (choice == 1) {
-      print("Enter start date (y-m-d): ");
-      DateTime startDate = DateTime.parse(stdin.readLineSync()!);
-      print("Enter end date (y-m-d): ");
-      DateTime endDate = DateTime.parse(stdin.readLineSync()!);
-      print("Enter room type (Single person or Double): ");
-      String roomType = stdin.readLineSync()!;
-      TypeMyRoom type;
-      switch (roomType) {
-        case "Single Person":
-          type = TypeMyRoom.singlePerson;
+      print("Enter seat type (FIRST_CLASS, BUSINESS_CLASS, ECONOMY_CLASS): ");
+      String? seatType = stdin.readLineSync();
+      SeatType type;
+      switch (seatType) {
+        case "FIRST_CLASS":
+          type = SeatType.FIRST_CLASS;
           break;
-        case "Double person":
-          type = TypeMyRoom.doublePerson;
+        case "BUSINESS_CLASS":
+          type = SeatType.BUSINESS_CLASS;
+          break;
+        case "ECONOMY_CLASS":
+          type = SeatType.ECONOMY_CLASS;
           break;
         default:
-          print("Invalid room type");
+          print("Invalid seat type");
           continue;
       }
-      hotel.makeReservation(startDate, endDate, type);
+      int seatNumber = flight.reserveSeat(type);
+      if (seatNumber == -1) {
+        print("No seats available");
+      } else {
+        print("Seat reserved: $seatNumber");
+      }
     } else if (choice == 2) {
-      hotel.displayReservations();
+      flight.displaySeats();
     } else if (choice == 3) {
       break;
     } else {
       print("Invalid choice");
-    }
-  }
-}
-
-enum TypeMyRoom {
-  singlePerson,
-  doublePerson,
-}
-
-class Room {
-  int? number;
-  TypeMyRoom? type;
-  double? rate;
-  bool? isPerfect;
-  Room(
-    this.number,
-    this.type,
-    this.rate,
-  ) {
-    this.isPerfect = true; //flag
-  }
-}
-
-class ReservationHotal {
-  int? id;
-  DateTime? startDate;
-  DateTime? endDate;
-  Room? room;
-  ReservationHotal(this.id, this.startDate, this.endDate, this.room) {
-    room!.isPerfect = false;
-  }
-  void display() {
-    print("Reservation ID: $id");
-    print("Start Date: $startDate");
-    print("End Date: $endDate");
-    print("Room Number: ${room!.number}");
-    print("Room Type: ${room!.type}");
-    print("Room Rate: ${room!.rate}");
-  }
-}
-
-class Hotel {
-  List<Room> rooms = [];
-  List<ReservationHotal> reservations = [];
-  int currantId = 0;
-  Hotel() {
-    rooms.add(Room(101, TypeMyRoom.singlePerson, 200.0));
-    rooms.add(Room(102, TypeMyRoom.singlePerson, 200.0));
-    rooms.add(Room(103, TypeMyRoom.singlePerson, 300.0));
-    rooms.add(Room(104, TypeMyRoom.doublePerson, 200.0));
-    rooms.add(Room(105, TypeMyRoom.doublePerson, 200.0));
-    rooms.add(Room(106, TypeMyRoom.doublePerson, 300.0));
-  }
-  Room? getRoom(TypeMyRoom type) {
-    for (var room in rooms) {
-      if (room.type == type && room.isPerfect!) {
-        return room;
-      }
-    }
-    return null;
-  }
-
-  void makeReservation(DateTime startDate, DateTime endDate, TypeMyRoom type) {
-    Room? room = getRoom(type);
-    if (room != null) {
-      ReservationHotal reservation =
-          ReservationHotal(++currantId, startDate, endDate, room);
-      reservations.add(reservation);
-    } else {
-      print("No room of $type");
-    }
-  }
-
-  void displayReservations() {
-    for (var reservation in reservations) {
-      reservation.display();
     }
   }
 }
